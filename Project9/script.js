5ed9e29d94e8bbc82dd0c4b648a8697bcb7e31d0
Ganaik Deshmukh <ganaikdeshmukh2004@gmail.com>
2023-08-09 13:27:02 +0530

    added the file

---
+++ Project9/script.js
@@ -0,0 +1,33 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("You Must Write Something!");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    savedData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        savedData();
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        savedData();
+    }
+});
+ function savedData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+ }
+ function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+ }
+ showTask();
