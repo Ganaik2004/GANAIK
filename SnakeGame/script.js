388f2889f349f779d24db5b61fd3c82974ffcedf
Ganaik Deshmukh <ganaikdeshmukh2004@gmail.com>
2023-08-18 12:33:23 +0530

    updated

--- SnakeGame/script.js
+++ SnakeGame/script.js
@@ -2,6 +2,12 @@ const playboard =document.querySelector(".play_board");
 let sco = document.querySelector(".score");
 let highScoreElement = document.querySelector(".highscore");
 let controls = document.querySelectorAll(".controls i");
+let level11 = document.querySelector(".selected");
+let select11 = document.querySelector(".select");
+const gamestart = new Audio("music/music.mp3");
+const gameover12 = new Audio("music/gameover.mp3");
+const move12 = new Audio("music/move.mp3");
+const food12 = new Audio("music/food.mp3");
 let gameover=false;
 let foodY,foodX;
 let SnakeX=5,SnakeY=10;
@@ -9,6 +15,7 @@ let VelocityX=0,VelocityY=0;
 let snakeBody=[];
 let setintervalid ;
 let score = 0;
+let speed;
 let highscore = localStorage.getItem("high_score") ||0;
 highScoreElement.innerHTML=`Hign Score : ${highscore}`;
 const changeFoodPOsition=()=>{
@@ -16,9 +23,11 @@ const changeFoodPOsition=()=>{
     foodY=Math.floor(Math.random()*30)+1;
 }
 function handlegameover(){
+    gameover12.play();
     clearInterval(setintervalid);
     alert("game over! Press enter to Start");
-    location.reload();
+   
+    location.reload(); 
 }
 const changedirection=(e)=>{
 if(e.key==="ArrowUp" && VelocityY!= 1){
@@ -43,15 +52,15 @@ const initGame = ()=>{
     if(gameover) return handlegameover();
     let htmlmarkup= `<div class ="food" style="grid-area:${foodY}/${foodX}"></div>`;
     if(SnakeX===foodX &&SnakeY===foodY){
+        food12.play();
         changeFoodPOsition();
         snakeBody.push([foodX,foodY]);
         score++;
-       
   highscore = score>=highscore? score : highscore;
   localStorage.setItem("high_score",highscore);
  sco.innerHTML=`Score : ${score}`;
  highScoreElement.innerHTML=`Hign Score : ${highscore}`;
-
+   move12.play();
     }
     for(let  i=snakeBody.length-1;i>0;i--){
         snakeBody[i]=snakeBody[i-1];
@@ -64,17 +73,20 @@ const initGame = ()=>{
     }
     for(let i=0;i<snakeBody.length;i++){
         htmlmarkup += `<div class ="head" style="grid-area:${snakeBody[i][1]}/${snakeBody[i][0]}"></div>`;
+      
         if(i!==0 && snakeBody[0][1]===snakeBody[i][1] && snakeBody[0][0]===snakeBody[i][0]){
             gameover=true;
         }
     }
   
     playboard.innerHTML=htmlmarkup;
+  select11.style.display="none";
   
 }
-changeFoodPOsition();
- setintervalid=setInterval(initGame,200);
-document.addEventListener("keydown",changedirection);
+
+
+
+
 let darkmode = document.querySelector(".dark-light-mode");
 let body = document.querySelector("body");
 let bright1 = document.querySelector(".bxs-sun");
@@ -86,3 +98,51 @@ darkmode.addEventListener("click", () => {
         bright1.classList.replace("bxs-moon", "bxs-sun")
     }
 });
+    const droupdowns = document.querySelectorAll('.dropdown');
+    droupdowns.forEach(dropdown=> {
+        const select = dropdown.querySelector('.select');
+        const caret = dropdown.querySelector('.caret');
+        const menu = dropdown.querySelector('.menu');
+        const options = dropdown.querySelectorAll('.menu li');
+        const selected = dropdown.querySelector('.selected');
+        select.addEventListener('click',()=>{
+            select.classList.toggle('select-clicked');
+            caret.classList.toggle('caret-rotate');
+            menu.classList.toggle('menu-open')
+        });
+        options.forEach(option =>{
+            option.addEventListener('click',()=>{
+                selected.innerText = option.innerText;
+                select.classList.remove('select-clicked');
+                caret.classList.remove('caret-rotate');
+                menu.classList.remove('menu-open')
+                if(level11.innerText==="Easy"){
+                    console.log(level11.innerText)
+                    speed = 500;
+                    changeFoodPOsition();
+                    setintervalid=setInterval(initGame,speed);
+                    gamestart.play();
+                document.addEventListener("keydown",changedirection);
+                   
+                }else if(level11.innerText==="Medium"){
+                    console.log(level11.innerText)
+                    speed = 300;
+                    changeFoodPOsition();
+                    setintervalid=setInterval(initGame,speed);
+                    gamestart.play();
+                document.addEventListener("keydown",changedirection);
+                }else if(level11.innerText==="Hard"){
+                    console.log(level11.innerText)
+                    speed = 100;
+                    changeFoodPOsition();
+                    setintervalid=setInterval(initGame,speed);
+                    gamestart.play();
+                document.addEventListener("keydown",changedirection);
+                }
+            })
+        })
+    
+    });
+
+
+
